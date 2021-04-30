520e909ae7e7fbf23beb3f0810eed6e098ffd369
mrgman <or.gotman@gmail.com>
2021-04-30 10:37:32 +0300

    Added initial MongoDB connection

--- mongo.js
+++ mongo.js
@@ -1,1 +1,27 @@
-// Set-up MongoDB
+let mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+
+const server = '127.0.0.1';
+const database = 'AnomalyDetectionDB';
+
+class Database {
+    constructor() {
+        this.connect()
+    }
+
+    connect() {
+        mongoose.connect(`mongodb://${server}/${database}`, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+            .then(() => {
+                console.log('Database connection successful')
+            })
+            .catch(err => {
+                console.error('Database connection error: ' + err)
+            })
+    }
+}
+
+module.exports = new Database()
+
