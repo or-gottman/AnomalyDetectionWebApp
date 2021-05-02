5bb4005d65df27ba6d2304c69877b01485a6787c
Talkleingit <klein125@gmail.com>
2021-05-02 20:20:23 +0300

    changed function name.

--- DataConverter.js
+++ DataConverter.js
@@ -8,27 +8,30 @@ class DataConverter
      * Returns an array with all rows as strings
      * @param data
      */
-    static convert(data) {
+    static toCsvFormat(data) {
         let keys = Object.keys(data);
         let colums = [];
         let rows = [];
-        rows.push(keys.toString());
+        let l = rows.push(keys.toString());
         for (let key in data) {
             colums.push(data[key])
         }
         let length = colums[0].length;      // all columns are of same length
         for (let i =0; i < length; i++) {
-            let tmp = []
+            let tmp = ''
             for (let col of colums) {
-                tmp.push(col[i]);
+                if (col === colums[colums.length-1]) {
+                    tmp += col[i];
+                    continue;
+                }
+                tmp +=  col[i] + ',';
             }
-            rows.push(tmp.toString());
+            rows.push(tmp);
         }
         return rows;
 
     }
 }
-
 
 module.exports = DataConverter;
 
